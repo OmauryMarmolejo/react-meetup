30283be456e6463aa5e31613922d9f0b9b26eb39
Omaury Marmolejo <omaury@tango.io>
2022-07-04 15:44:22 -0500

    Adding react project

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+
+function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-106d4-default-rtdb.firebaseio.com/meetups.json"
+    ).then((response) => {
+      response.json().then((data) => {
+        console.log(data);
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+export default AllMeetupsPage;
